b36ac9c19b7c2196b0b7658bc5d66612bafdb4d6
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-10-23 22:47:51 -0500

    feat: user dto

---
+++ backend/src/main/java/dto/model/UserDto.java
@@ -0,0 +1,27 @@
+package dto.model;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@ToString
+public class UserDto {
+
+    private Long id;
+
+    private String name;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private Boolean isAdmin;
+
+    private Long institutionId;
+
+}
