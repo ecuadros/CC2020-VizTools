2634c78bf845ea10f1fa7d6982a8352a5088478b
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-10-23 22:48:15 -0500

    feat: auth dto

---
+++ backend/src/main/java/dto/model/AuthTokenDto.java
@@ -0,0 +1,21 @@
+package dto.model;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@ToString
+public class AuthTokenDto {
+
+    private Long id;
+
+    private String name;
+    
+    private String token;
+    
+    private Boolean isAdmin;
+
+}
