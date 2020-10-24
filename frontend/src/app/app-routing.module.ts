31f3516e30655d1020e6d3e1c0076c551e0b18f6
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-10-24 00:02:32 -0500

    feat($routes): routes added
    - public pages: '/..'
    - authentication pages: '/auth/..'
    - admin pages: '/admin/..'

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,21 @@ import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./pages/pages.module')
+      .then(m => m.PagesModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module')
+      .then(m => m.AuthModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module')
+      .then(m => m.AdminModule),
+  },
+  {
+    path: '',
     redirectTo: '',
     pathMatch: 'full'
   },
