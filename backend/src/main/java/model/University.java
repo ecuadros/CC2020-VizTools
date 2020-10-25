2ea9fc1b02e81278bca4c44472949e1af3d6a276
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-10-24 19:54:38 -0500

    fix: country id added to university constraint

--- backend/src/main/java/model/University.java
+++ backend/src/main/java/model/University.java
@@ -14,7 +14,7 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @Entity
 @Table(name = "university", uniqueConstraints = {
-    @UniqueConstraint(columnNames = { "name" })
+    @UniqueConstraint(columnNames = { "name", "country_id" })
 })
 public class University {
     
