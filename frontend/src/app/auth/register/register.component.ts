ca89bb1b59d4e3b44b5885a977ff3ecc3b3a394e
Carlos Cupe <carlos.cupe@utec.edu.pe>
2020-11-09 22:48:04 -0500

    fix: register form updated
    American format

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -10,19 +10,20 @@ import { AlertService, AuthService, CountryService, UniversityService } from 'sr
 })
 export class RegisterComponent implements OnInit {
 
-  registerForm: FormGroup;
-  universityRegForm: FormGroup;
-  universitySelForm: FormGroup;
+  userForm: FormGroup;
+  infoForm: FormGroup;
+
   submitted = false;
   returnUrl: string;
   universitySection = false;
-  universitySelection = true;
-
-  universityPD: any;
-  countryPD: any;
+  
+  universityPD = [];
+  countryPD = [];
 
   countryFilterPD: any;
   countrySelected = false;
+
+  defaultDomain: string;
 
   options = {
     autoClose: true,
@@ -40,23 +41,25 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.registerForm = this.formBuilder.group({
+    this.userForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(18)]]
     });
 
-    this.universityRegForm = this.formBuilder.group({
-      name: ['', Validators.required],
+    this.infoForm = this.formBuilder.group({
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipCode: ['', Validators.required],
+      phone: ['', Validators.required],
+      whatsApp: [false],
+      telegram: [false],
+      university: ['', Validators.required],
       acronym: ['', Validators.required],
       domain: ['', Validators.required],
       country: ['', Validators.required],
-      occupation: ['', Validators.required]
-    });
-
-    this.universitySelForm = this.formBuilder.group({
-      university: ['', Validators.required],
+      countryUniversity: ['', Validators.required],
       occupation: ['', Validators.required]
     });
 
@@ -68,59 +71,53 @@ export class RegisterComponent implements OnInit {
   }
 
   onUserRegister() {
-    this.universitySection = true;
+    let email = this.userControl.email.value;
+    this.authService.isEmailRegistered(email).then(
+      response => {
+        this.universitySection = true;
+      }, error => {
+        this.alertService.error(error.error, this.options);
+      });
+
+    let i = email.indexOf("@");
+    let url = 'wwww.' + email.substring(i + 1);
+    this.defaultDomain = url;
+    this.infoControl.domain.setValue(url);
   }
 
-  onUniversityRegister() {
+  onCompleteRegister() {
     this.submitted = true;
+    let university = this.universityPD.find(x => x.id == this.infoControl.university.value);
     let form = {
       user: {
-        name: this.regForm.firstName.value,
-        lastName: this.regForm.lastName.value,
-        email: this.regForm.email.value,
-        password: this.regForm.password.value,
-        occupation: this.urForm.occupation.value
+        name: this.userControl.firstName.value,
+        lastName: this.userControl.lastName.value,
+        email: this.userControl.email.value,
+        password: this.userControl.password.value
+      },
+      userInfo: {
+        country: this.infoControl.country.value,
+        city: this.infoControl.city.value,
+        state: this.infoControl.state.value,
+        zipCode: this.infoControl.zipCode.value,
+        phone: this.infoControl.phone.value,
+        whatsApp: this.infoControl.whatsApp.value,
+        telegram: this.infoControl.telegram.value,
+        occupation: this.infoControl.occupation.value,
       },
       university: {
-        name: this.urForm.name.value,
-        acronym: this.urForm.acronym.value,
-        url: this.urForm.domain.value,
-        countryId: this.urForm.country.value,
+        id: this.infoControl.university.value,
+        name: university.name,
+        acronym: this.infoControl.acronym.value,
+        url: this.infoControl.domain.value,
+        countryId: this.infoControl.country.value,
       },
-      isUniversityRegister: false
     }
     this.authService.register(form).subscribe(
       data => {
         this.router.navigate([this.returnUrl]);
         },
       error => {
-        this.universitySection = false;
-        this.alertService.error(error.error, this.options);
-        this.submitted = false;
-      }
-    );
-  }
-
-  onUniversitySelect() {
-    this.submitted = true;
-    let form = {
-      user: {
-        name: this.regForm.firstName.value,
-        lastName: this.regForm.lastName.value,
-        email: this.regForm.email.value,
-        password: this.regForm.password.value,
-        occupation: this.urForm.occupation.value,
-        universityId: this.usForm.university.value
-      },
-      university: null,
-      isUniversityRegister: true
-    }
-    this.authService.register(form).subscribe(
-      data => {
-        this.router.navigate([this.returnUrl]);
-        },
-      error => {
-        this.universitySection = false;
         this.alertService.error(error.error, this.options);
         this.submitted = false;
       }
@@ -128,29 +125,50 @@ export class RegisterComponent implements OnInit {
   }
 
   onCountrySelected(e) {
+    this.infoControl.countryUniversity.setValue(e.value);
     this.universityService.readByCountry(e.value).then(
       response => { this.universityPD = response; this.countrySelected = true; }
     );
+  }
+  
+  onCountryUniversitySelected(e) {
+    this.universityService.readByCountry(e.value).then(
+      response => { this.universityPD = response; this.countrySelected = true; }
+    );
+  }
+
+  onUniversitySelected(e) {
+    let university = this.universityPD.find(x => x.id == e.value);
+    this.infoControl.acronym.setValue(university.acronym);
+    this.infoControl.domain.setValue(university.url);
+  }
+
+  onNewUniversity(e) {
+    e.customItem = { id: -1, name: e.text };
+
+    let newUniversity = this.universityPD.find(x => x.id == -1);
+
+    if (newUniversity == undefined) {
+      this.universityPD.push(e.customItem);
+    } else {
+      newUniversity.name = e.text;
+    }
+
+    this.infoControl.university.setValue(-1);
+    this.infoControl.acronym.reset();
+    this.infoControl.domain.setValue(this.defaultDomain);
   }
 
   backForm() {
     this.universitySection = false;
   }
 
-  changeUniversityView() {
-    this.universitySelection = !this.universitySelection;
+  get userControl() {
+    return this.userForm.controls;
   }
 
-  get regForm() {
-    return this.registerForm.controls;
-  }
-
-  get urForm() {
-    return this.universityRegForm.controls;
-  }
-
-  get usForm() {
-    return this.universitySelForm.controls;
+  get infoControl() {
+    return this.infoForm.controls;
   }
 
 }
