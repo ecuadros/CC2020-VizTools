31e63c5d5bbd5458fec6661cc5a0930006792269
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-18 14:05:03 -0500

    refactor: unused libraries removed

--- backend/src/main/java/ServiceApplication.java
+++ backend/src/main/java/ServiceApplication.java
@@ -1,16 +1,9 @@
-import java.io.File;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import security.Constants;
 
 @SpringBootApplication
 @ComponentScan({"controller", "service", "config", "security", "exception" })
