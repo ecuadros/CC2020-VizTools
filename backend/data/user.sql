0fd53fa7fa4dbd6e316d758c09392b05fa5cda15
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-18 13:59:04 -0500

    feat: basic admin user example

--- backend/data/user.sql
+++ backend/data/user.sql
@@ -2,7 +2,8 @@ INSERT INTO public.role(id, name) VALUES (1, 'ROLE_USER');
 INSERT INTO public.role(id, name) VALUES (2, 'ROLE_PM');
 INSERT INTO public.role(id, name) VALUES (3, 'ROLE_ADMIN');
 
-INSERT INTO public.users(id, email, password, name, last_name) VALUES (1, 'admin@cs.com', '$2a$10$5kJmRwMcOZLCjC1ZB56HB.aJCB9wktE593b58EYdDDsaZOhBgWGIi', 'Admin', 'CS');
+-- Password: "qwerty"
+INSERT INTO public.users(id, email, password, first_name, last_name, enabled) VALUES (1, 'computing.curricula@gmail.com', '$2a$10$5kJmRwMcOZLCjC1ZB56HB.aJCB9wktE593b58EYdDDsaZOhBgWGIi', 'Admin', 'CS', true);
 
 INSERT INTO public.user_roles(user_id, role_id) VALUES (1, 3);
 
