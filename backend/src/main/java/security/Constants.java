565114905554d33ae425a70e523d88d1589c553c
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-18 23:55:50 -0500

    feat: frontend url

--- backend/src/main/java/security/Constants.java
+++ backend/src/main/java/security/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String SIGNING_KEY = "";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
+    public static final String FRONTEND_URL = "https://cc.spc.org.pe";
 }
