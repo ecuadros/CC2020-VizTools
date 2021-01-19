313eee2c915073e69d65e57f0c6e13f52d4119a4
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-18 23:56:42 -0500

    feat: verification token validity

--- backend/src/main/java/security/Constants.java
+++ backend/src/main/java/security/Constants.java
@@ -1,6 +1,7 @@
 package security;
 
 public class Constants {
+    public static final int VERIFICATION_TOKEN_VALIDITY_MINUTES = 24*60;
     public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
     public static final String SIGNING_KEY = "";
     public static final String TOKEN_PREFIX = "Bearer ";
