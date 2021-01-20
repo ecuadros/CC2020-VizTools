bbf470d3e24ac0b5dd0e8efa6c3d446b682ab868
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-19 23:33:02 -0500

    feat: added user information and login inline

--- frontend/src/app/@core/components/header/header.component.ts
+++ frontend/src/app/@core/components/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/@core/services';
+import { AuthService, NavbarService } from 'src/app/@core/shared/services';
 
 @Component({
   selector: 'app-header',
@@ -9,15 +9,21 @@ import { AuthService } from 'src/app/@core/services';
 })
 export class HeaderComponent implements OnInit {
 
-  @Input() navItems: any;
+  navItems: any[] = [];
 
   isAuthenticated: boolean = false;
 
-  userName = "";
-  universityName = "";
-  email = "";
+  userName: string = '';
+  universityName: string = '';
+  email: string = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private navbarService: NavbarService
+  ) {
+    this.navItems = this.navbarService.getRoutes();
+  }
 
   ngOnInit(): void {
     this.isAuthenticated = this.authService.isAuthenticated();
@@ -28,6 +34,8 @@ export class HeaderComponent implements OnInit {
 
   logout(): void {
     this.authService.logout();
+    this.isAuthenticated = false;
+    this.navItems = this.navbarService.getRoutes();
     this.router.navigate(['/']);
   }
 
