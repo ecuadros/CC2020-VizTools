c3e62c2ab5eb5abb4cb1664a404db9ddec0cfc8a
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-20 14:21:08 -0500

    feat: default language assignment

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateConfigService } from './@core/shared/services';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+
+  constructor(
+    private translateService: TranslateConfigService
+  ) {
+    this.translateService.setDefaultLang('en');
+  }
+
 }
