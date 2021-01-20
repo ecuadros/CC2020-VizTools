e2365e97b86d3985ac6b17d6964747095183efc8
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-20 11:04:50 -0500

    feat: added routes and imported components

--- frontend/src/app/auth/auth-routing.module.ts
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ActivateAccountComponent } from './activate-account/activate-account.component';
+import { ResendEmailComponent } from './resend-email/resend-email.component';
 import { AuthGuard } from '../@core/guards';
 
 const routes: Routes = [{
@@ -17,6 +19,14 @@ const routes: Routes = [{
     {
       path: 'register',
       component: RegisterComponent,
+    },
+    {
+      path: 'activate-account/:token',
+      component: ActivateAccountComponent,
+    },
+    {
+      path: 'resend-email',
+      component: ResendEmailComponent,
     },
     {
       path: '',
--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -4,9 +4,16 @@ import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { DevExtremeModule } from 'devextreme-angular';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CoreModule } from '../@core/core.module';
 import { AuthComponent } from './auth.component';
+import { ActivateAccountComponent } from './activate-account/activate-account.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ResendEmailComponent } from './resend-email/resend-email.component';
+import { CredentialsFormComponent } from './register/credentials-form/credentials-form.component';
+import { InfoFormComponent } from './register/info-form/info-form.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 
@@ -14,14 +21,25 @@ import { AuthComponent } from './auth.component';
   declarations: [
     AuthComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ActivateAccountComponent,
+    ResendEmailComponent,
+    CredentialsFormComponent,
+    InfoFormComponent
   ],
   imports: [
     CommonModule,
     AuthRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
     DevExtremeModule,
-    CoreModule
+    NgxSpinnerModule,
+    FontAwesomeModule,
+    CoreModule,
+    TranslateModule
+  ],
+  exports: [
+    CredentialsFormComponent
   ]
 })
 export class AuthModule { }
