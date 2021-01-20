0be086f49f807bfa3a8f1538d4ee8768ddc32bb1
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-20 14:12:44 -0500

    feat: user comparison tool

---
+++ frontend/src/app/user/compare-tool/compare-tool.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CompareToolComponent } from './compare-tool.component';
+
+describe('CompareToolComponent', () => {
+  let component: CompareToolComponent;
+  let fixture: ComponentFixture<CompareToolComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CompareToolComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CompareToolComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/user/compare-tool/compare-tool.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService, TableService, ShareGraphService, DKAService } from 'src/app/@core/shared/services';
+import { CProgram, UProgram, Series, DKA, CWeight, UWeight } from 'src/app/@core/models';
+
+@Component({
+  selector: 'app-compare-tool',
+  templateUrl: './compare-tool.component.html',
+  styleUrls: ['./compare-tool.component.scss']
+})
+export class CompareToolComponent implements OnInit {
+
+  dkas: DKA[] = [];
+
+  selectedCPrograms: Series[] = [];
+  selectedUPrograms: Series[] = [];
+
+  selectedSeries: Series[] = [];
+
+  graphLink: string = '';
+  universityId: number;
+
+  optionsUserPrograms: any = {
+    filter: (series: Series): boolean => {
+      return series.universityId === this.universityId;
+    }
+  };
+
+  optionsUPrograms: any = {
+    filter: (series: Series): boolean => {
+      return series.universityId !== this.universityId;
+    }
+  };
+
+  constructor(
+    private authService: AuthService,
+    private dkaService: DKAService,
+    private tableService: TableService,
+    private shareGraphService: ShareGraphService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.universityId = this.authService.universityId;
+    this.dkaService.readAll().subscribe(
+      (dkas: DKA[]) => { this.dkas = dkas }
+    );
+    let routeData = this.route.snapshot.queryParamMap.get('data');
+    if (routeData != undefined) {
+      this.shareGraphService.importGraph(routeData, this.selectedCPrograms, this.selectedUPrograms);
+    }
+    this.shareGraphService.onComplete$.subscribe(() => this.updateGraph());
+  }
+
+  onSelectedCProgram(program: CProgram): void {
+    if (this.isProgramSelected(program)) return;
+    this.tableService.readByCProgram(program.id).subscribe(
+      (weights: CWeight[]) => {
+        let serie = new Series(program, weights, this.color);
+        this.selectedCPrograms.push(serie);
+        this.updateGraph();
+      }
+    );
+  }
+
+  onSelectedUProgram(program: UProgram): void {
+    if (this.isProgramSelected(program)) return;
+    this.tableService.readByUProgram(program.id).subscribe(
+      (weights: UWeight[]) => {
+        let serie = new Series(program, weights, this.color);
+        this.selectedUPrograms.push(serie);
+        this.updateGraph();
+      }
+    );
+  }
+
+  isProgramSelected(program: CProgram | UProgram): boolean {
+    let item;
+    if (program instanceof CProgram) {
+      item = this.selectedCPrograms.find(item => item.id === program.formatedId);
+    }
+    if (program instanceof UProgram) {
+      item = this.selectedUPrograms.find(item => item.id === program.formatedId);
+    }
+    return item !== undefined;
+  }
+
+  updateGraph(): void {
+    this.selectedSeries = this.selectedCPrograms.concat(this.selectedUPrograms);
+    this.graphLink = this.shareGraphService.exportGraph(this.selectedCPrograms, this.selectedUPrograms);
+  }
+
+  get color() {
+    return '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6)
+  }
+
+}
