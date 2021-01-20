ebe8ba09634a9da9a287f0b6c763747fb0bff2a1
Carlos Cupe <carlos.cupe@utec.edu.pe>
2021-01-19 23:27:56 -0500

    refactor: dependency path update

--- frontend/src/app/@core/guards/auth.guard.ts
+++ frontend/src/app/@core/guards/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivateChild,} from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../shared/services/auth.service';
 
 @Injectable({
   providedIn: 'root',
