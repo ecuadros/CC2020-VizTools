c28ace3e9a33c2444aecb16b9a553e24c2ddaedc
Carlos Cupe <carlos.cupe.g@gmail.com>
2023-03-13 11:32:00 -0500

    fix: format correction

--- backend/db/user.sql
+++ backend/db/user.sql
@@ -1,4 +1,4 @@
 -- Password: "qwerty"
-INSERT INTO public._user(id, email, password, first_name, last_name, is_account_non_locked, is_enabled, role) VALUES (1, 'computing.curricula@gmail.com', '$2a$10$5kJmRwMcOZLCjC1ZB56HB.aJCB9wktE593b58EYdDDsaZOhBgWGIi', 'Admin', 'CS', true, true, 'ADMIN')
+INSERT INTO public._user(id, email, password, first_name, last_name, is_account_non_locked, is_enabled, role) VALUES (1, 'computing.curricula@gmail.com', '$2a$10$5kJmRwMcOZLCjC1ZB56HB.aJCB9wktE593b58EYdDDsaZOhBgWGIi', 'Admin', 'CS', true, true, 'ADMIN');
 
-ALTER SEQUENCE _user_id_seq RESTART WITH 2
+ALTER SEQUENCE _user_id_seq RESTART WITH 2;
